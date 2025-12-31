bb711459391a249ffa67b2a87b32002b88fa972f
Bryan Porter <bryan@bryanporter.com>
2025-12-31 07:53:32 -0600

    fixed typo

--- src/data/categories.ts
+++ src/data/categories.ts
@@ -1,7 +1,7 @@
 import type { Category } from '../types';
 
 const categories = [
-  { tenantId: 1, categoryId: 1, name: 'Motorolo 68000 Computers', description: 'Some long descriptive text about this particular category.', parentCategoryId: null },
+  { tenantId: 1, categoryId: 1, name: 'Motorola 68000 Computers', description: 'Some long descriptive text about this particular category.', parentCategoryId: null },
   { tenantId: 1, categoryId: 2, name: 'Compact Macintosh', description: 'Some long descriptive text about this particular category.', parentCategoryId: 1 },
   { tenantId: 1, categoryId: 3, name: 'Sun Workstations', description: 'Some long descriptive text about this particular category.', parentCategoryId: 1 },
   { tenantId: 1, categoryId: 4, name: 'Desktop Macintosh', description: 'Some long descriptive text about this particular category.', parentCategoryId: 1 },
