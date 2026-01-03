d7acd09212343844fb364d87c4855056ea24f725
Bryan Porter <bryan@bryanporter.com>
2026-01-02 22:25:34 -0600

    reorganized project into web and api, set up dev proxy for frontend

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -4,6 +4,16 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5147/',
+        changeOrigin: true,            // necessary for name-based virtual hosted sites
+        secure: false,                 // if you want to bypass SSL certificate checks
+        rewrite: (path) => path.replace(/^\/api/, '') // remove /auth from the final URL
+      }
+    }
+  },
   test: {
     globals: true,
     environment: 'jsdom',
